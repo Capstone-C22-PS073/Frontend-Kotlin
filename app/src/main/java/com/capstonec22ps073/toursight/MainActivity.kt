c39f7265cc93caa4b7a2c7a770c7dab52eb96296
Ario Bagus Bramantyo <i.ario.bramantyo@gmail.com>
2022-05-17 13:02:19 +0700

    first init

---
+++ app/src/main/java/com/capstonec22ps073/toursight/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.capstonec22ps073.toursight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
