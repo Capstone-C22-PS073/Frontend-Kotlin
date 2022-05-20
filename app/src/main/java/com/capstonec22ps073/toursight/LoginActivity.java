34c3a2e06d280617e5d697bcb573ba79ba395b7f
bernadustito <titonurhadi27@gmail.com>
2022-05-20 13:57:24 +0700

    membuat regristasi terbaru

--- app/src/main/java/com/capstonec22ps073/toursight/LoginActivity.java
+++ app/src/main/java/com/capstonec22ps073/toursight/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.capstonec22ps073.toursight;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.capstonec22ps073.toursight.databinding.ActivityLoginBinding;
@@ -11,9 +14,15 @@ import com.capstonec22ps073.toursight.databinding.OnboardingItemContainerBinding
 
 
 public class LoginActivity extends Activity {
+    Button btnRegister;
 protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+    btnRegister = findViewById(R.id.btn_register);
+    btnRegister.setOnClickListener(view -> {
+        Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(i);
+    });
 }
 
     }
